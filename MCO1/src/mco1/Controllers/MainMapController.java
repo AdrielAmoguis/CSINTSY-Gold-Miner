80f6bd2ee5af796566aab0c5a3b884fb29f2ef12
Adriel Isaiah Amoguis <adriel_amoguis@dlsu.edu.ph>
2020-12-04 17:24:20 +0800

    Creating the MainMap

--- MCO1/src/mco1/Controllers/MainMapController.java
+++ MCO1/src/mco1/Controllers/MainMapController.java
@@ -1,5 +1,6 @@
 package mco1.Controllers;
 
+import com.sun.prism.paint.Color;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -8,21 +9,66 @@ import javafx.event.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+
+import javax.security.auth.login.AccountNotFoundException;
 
 public class MainMapController implements EventHandler<Event>
 {
-    // JavaFX Instances
+    // Size N
+    private int dimension;
 
+    // JavaFX Instances
+    @FXML
+    GridPane mainGridPane;
+
+    private Pane[][] panes;
 
     public MainMapController(int n)
     {
-
+        this.dimension = n;
     }
 
     @FXML
     public void initialize()
     {
+        // Create the panes
+        panes = new Pane[this.dimension][this.dimension];
 
+        // Clear the GridPane
+
+        for(int i = 0; i < this.dimension; i++)
+            for(int j = 0; j < this.dimension; j++)
+            {
+                panes[i][j] = new Pane();
+                panes[i][j].setStyle("-fx-background-color: #32a852; -fx-border-color: black; -fx-border-width: 1;");
+            }
+
+        // Bind the Panes
+        for(int i = 0; i < this.dimension; i++)
+        {
+            mainGridPane.addRow(i, panes[i]);
+        }
+
+        // Resize Everything
+        for(int i = 0; i < this.dimension; i++)
+        {
+            double scale = 100 / this.dimension;
+            RowConstraints rowConstraints = new RowConstraints();
+            rowConstraints.setPercentHeight(scale);
+            mainGridPane.getRowConstraints().add(rowConstraints);
+        }
+
+        for(int i = 0; i < this.dimension; i++)
+        {
+            double scale = 100 / this.dimension;
+            ColumnConstraints columnConstraints = new ColumnConstraints();
+            columnConstraints.setPercentWidth(scale);
+            mainGridPane.getColumnConstraints().add(columnConstraints);
+        }
     }
 
 
