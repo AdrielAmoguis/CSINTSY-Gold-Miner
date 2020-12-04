b53ed18076dea8eca416dd44bda85401bc68af1c
Adriel Isaiah Amoguis <adriel_amoguis@dlsu.edu.ph>
2020-12-04 22:37:20 +0800

    Updated the MainPane

--- MCO1/src/mco1/Controllers/MainMapController.java
+++ MCO1/src/mco1/Controllers/MainMapController.java
@@ -1,34 +1,46 @@
 package mco1.Controllers;
 
-import com.sun.prism.paint.Color;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import mco1.Model.Board;
 import javafx.fxml.FXML;
 import javafx.event.*;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.layout.*;
 import javafx.scene.control.*;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.RowConstraints;
-
-import javax.security.auth.login.AccountNotFoundException;
 
 public class MainMapController implements EventHandler<Event>
 {
     // Size N
     private int dimension;
 
+    // Board Instance
+    private Board mainBoard;
+
     // JavaFX Instances
     @FXML
+    AnchorPane mainPane;
+
+    @FXML Button setGoldButton;
+    @FXML Button setPitButton;
+    @FXML Button setBeaconButton;
+    @FXML CheckBox checkGold;
+    @FXML CheckBox checkBeacon;
+    @FXML CheckBox checkPit;
+    @FXML CheckBox checkSlowMode;
+    @FXML Button startSimulationButton;
+    @FXML ComboBox comboAlgo;
+    @FXML Label counterDisplay;
+
+
     GridPane mainGridPane;
+
+    private int initState;
 
     private Pane[][] panes;
 
     public MainMapController(int n)
     {
+        this.mainBoard = new Board(n);
+        this.initState = 0;
         this.dimension = n;
     }
 
@@ -38,25 +50,17 @@ public class MainMapController implements EventHandler<Event>
         // Create the panes
         panes = new Pane[this.dimension][this.dimension];
 
-        // Clear the GridPane
+        // Create the GridPane
+        this.mainGridPane = new GridPane();
+        mainGridPane.setPrefHeight(773);
+        mainGridPane.setPrefWidth(1302);
+        mainGridPane.setPadding(new Insets(10, 10, 10, 10));
+        mainPane.getChildren().add(mainGridPane);
 
-        for(int i = 0; i < this.dimension; i++)
-            for(int j = 0; j < this.dimension; j++)
-            {
-                panes[i][j] = new Pane();
-                panes[i][j].setStyle("-fx-background-color: #32a852; -fx-border-color: black; -fx-border-width: 1;");
-            }
-
-        // Bind the Panes
+        // Set Size Constraints
         for(int i = 0; i < this.dimension; i++)
         {
-            mainGridPane.addRow(i, panes[i]);
-        }
-
-        // Resize Everything
-        for(int i = 0; i < this.dimension; i++)
-        {
-            double scale = 100 / this.dimension;
+            double scale = mainGridPane.getPrefHeight() / this.dimension;
             RowConstraints rowConstraints = new RowConstraints();
             rowConstraints.setPercentHeight(scale);
             mainGridPane.getRowConstraints().add(rowConstraints);
@@ -64,10 +68,24 @@ public class MainMapController implements EventHandler<Event>
 
         for(int i = 0; i < this.dimension; i++)
         {
-            double scale = 100 / this.dimension;
+            double scale = mainGridPane.getPrefWidth() / this.dimension;
             ColumnConstraints columnConstraints = new ColumnConstraints();
             columnConstraints.setPercentWidth(scale);
             mainGridPane.getColumnConstraints().add(columnConstraints);
+        }
+
+        for(int i = 0; i < this.dimension; i++)
+            for(int j = 0; j < this.dimension; j++)
+            {
+                panes[i][j] = new Pane();
+                panes[i][j].setOnMousePressed(this::handle);
+                panes[i][j].setStyle("-fx-border-color: black; -fx-border-width: 1;");
+            }
+
+        // Bind the Panes
+        for(int i = 0; i < this.dimension; i++)
+        {
+            mainGridPane.addRow(i, panes[i]);
         }
     }
 
@@ -77,11 +95,37 @@ public class MainMapController implements EventHandler<Event>
     {
         if(ev instanceof ActionEvent)
             handle((ActionEvent) ev);
+
+        // Pane Press
+        else if(ev.getSource() instanceof Pane)
+        {
+            if(this.initState == 1)
+            {
+                // Set the Gold Pot
+            }
+            else if(this.initState == 2)
+            {
+                // Set the Pits
+            }
+            else if(this.initState == 3)
+            {
+                // Set Beacons
+            }
+        }
     }
 
     // ActionEvent Handler
     private void handle(ActionEvent ev)
     {
+        // Check for Button
+        if(ev.getSource() instanceof Button)
+        {
+            // Button Handler
+            if(((Button) ev.getSource()).getId().equals(setGoldButton.getId()))
+            {
+                this.initState = 1;
+            }
 
+        }
     }
 }
