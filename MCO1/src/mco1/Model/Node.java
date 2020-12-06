bee9cebc08a5798ddcb822b33f959222018f8589
Ben Sun <ventilantum@gmail.com>
2020-12-06 22:38:47 +0800

    Added status attribute in Board class

---
+++ MCO1/src/mco1/Model/Node.java
@@ -0,0 +1,58 @@
+package mco1.Model;
+import mco1.Model.Locations.*;
+
+public class Node {
+    private Location location;
+    private Node parent;
+    private boolean isVisited;
+
+    /**
+     * Constructor for root Node. Creates a Node with empty parent.
+     * @param location Location to be stored in stack
+     */
+    public Node(Location location){
+        this.location = location;
+        isVisited = false;
+    }
+
+    /**
+     * Constructor for child Node. Creates a Node with existing parent.
+     * @param location Location to be stored in stack
+     * @param parent Parent of node, to be used in backtracking.
+     */
+    public Node(Location location, Node parent){
+        this(location);
+        this.parent = parent;
+    }
+
+    /**
+     * Returns the Location stored in this Node.
+     * @return the Location stored in this Node.
+     */
+    public Location getLocation(){
+        return location;
+    }
+
+    /**
+     * Returns the parent (if any) of this Node.
+     * @return the parent of this Node (if any)
+     */
+    public Node getParent(){
+        return parent;
+    }
+
+    /**
+     * Returns true if the Node has been visited.
+     * @return true if the Node has been visited
+     */
+    public boolean getStatus(){
+        return isVisited;
+    }
+
+    /**
+     * Sets this node as visited.
+     */
+    public void visit(){
+        isVisited = true;
+    }
+}
